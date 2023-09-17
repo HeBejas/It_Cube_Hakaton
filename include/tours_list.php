7eb7db46baef5c735baacf3003a0261b1894d4df
maximkasvetofor <maximplay2018@gmail.com>
2023-09-17 13:36:19 +0400

    izhgo v0.6
    ниче не поменялось

--- include/tours_list.php
+++ include/tours_list.php
@@ -1,94 +1,37 @@
-<div class="tour" style="background: url(media/images/natsionalnyy-park.png);">
-    <div class="tour_place">Ижевск</div>
-    <div class="tour_info">
-        <div class="flex jc_space-between">
-            <div class="tour_info_name">Зуевы Ключи</div>
-            <div>
-                <div class="tour_info_time">12 часов</div>
-                <div class="tour_info_price">2900₽</div>
-            </div>
-        </div>
-        <div class="tour_desc">Одно из красивейших мест Удмуртии находится в Каракулинском района. Его особенностью является пейзаж в виде просторов Камы.</div>
-    </div>
-</div>
-<div class="tour" style="background: url(media/images/natsionalnyy-park.png);">
-    <div class="tour_place">Ижевск</div>
-    <div class="tour_info">
-        <div class="flex jc_space-between">
-            <div class="tour_info_name">Зуевы Ключи</div>
-            <div>
-                <div class="tour_info_time">12 часов</div>
-                <div class="tour_info_price">2900₽</div>
-            </div>
-        </div>
-        <div class="tour_desc">Одно из красивейших мест Удмуртии находится в Каракулинском района. Его особенностью является пейзаж в виде просторов Камы.</div>
-    </div>
-</div>
-<div class="tour" style="background: url(media/images/natsionalnyy-park.png);">
-    <div class="tour_place">Ижевск</div>
-    <div class="tour_info">
-        <div class="flex jc_space-between">
-            <div class="tour_info_name">Зуевы Ключи</div>
-            <div>
-                <div class="tour_info_time">12 часов</div>
-                <div class="tour_info_price">2900₽</div>
-            </div>
-        </div>
-        <div class="tour_desc">Одно из красивейших мест Удмуртии находится в Каракулинском района. Его особенностью является пейзаж в виде просторов Камы.</div>
-    </div>
-</div>
-<div class="tour" style="background: url(media/images/natsionalnyy-park.png);">
-    <div class="tour_place">Ижевск</div>
-    <div class="tour_info">
-        <div class="flex jc_space-between">
-            <div class="tour_info_name">Зуевы Ключи</div>
-            <div>
-                <div class="tour_info_time">12 часов</div>
-                <div class="tour_info_price">2900₽</div>
-            </div>
-        </div>
-        <div class="tour_desc">Одно из красивейших мест Удмуртии находится в Каракулинском района. Его особенностью является пейзаж в виде просторов Камы.</div>
-    </div>
-</div>
-<div class="tour" style="background: url(media/images/natsionalnyy-park.png);">
-    <div class="tour_place">Ижевск</div>
-    <div class="tour_info">
-        <div class="flex jc_space-between">
-            <div class="tour_info_name">Зуевы Ключи</div>
-            <div>
-                <div class="tour_info_time">12 часов</div>
-                <div class="tour_info_price">2900₽</div>
-            </div>
-        </div>
-        <div class="tour_desc">Одно из красивейших мест Удмуртии находится в Каракулинском района. Его особенностью является пейзаж в виде просторов Камы.</div>
-    </div>
-</div>
-<div class="tour" style="background: url(media/images/natsionalnyy-park.png);">
-    <div class="tour_place">Ижевск</div>
-    <div class="tour_info">
-        <div class="flex jc_space-between">
-            <div class="tour_info_name">Зуевы Ключи</div>
-            <div>
-                <div class="tour_info_time">12 часов</div>
-                <div class="tour_info_price">2900₽</div>
-            </div>
-        </div>
-        <div class="tour_desc">Одно из красивейших мест Удмуртии находится в Каракулинском района. Его особенностью является пейзаж в виде просторов Камы.</div>
-    </div>
-</div>
-<div class="tour" style="background: url(media/images/natsionalnyy-park.png);">
-    <div class="tour_place">Ижевск</div>
-    <div class="tour_info">
-        <div class="flex jc_space-between">
-            <div class="tour_info_name">Зуевы Ключи</div>
-            <div>
-                <div class="tour_info_time">12 часов</div>
-                <div class="tour_info_price">2900₽</div>
-            </div>
-        </div>
-        <div class="tour_desc">Одно из красивейших мест Удмуртии находится в Каракулинском района. Его особенностью является пейзаж в виде просторов Камы.</div>
-    </div>
-</div>
+<?php
+    $query = "SELECT tour.id_tour, tour.name_tour, tour.price_tour, typetour.name_typetour, tour.duration_tour, tour.desc_tour, tour.img_tour 
+        FROM `tour`
+        LEFT JOIN `typetour` ON tour.id_typetour=typetour.id_typetour
+        WHERE 1";
+    include ("connect.php");
+    $link = mysqli_connect($host, $user, $password, $db_name); 
+    $result = mysqli_query($link, $query);
+    if($result == false) {
+        die("Произошла ошибка при выполнении запроса"); 
+    }
+    else {
+        for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+        $compare = "";
+        foreach ($data as $row) {
+            echo 
+                "
+                <div class='tour' style='background: url($row[img_tour]);' onclick='transition($row[id_tour])'>
+                    <div class='tour_place'>$row[name_typetour]</div>
+                    <div class='tour_info'>
+                        <div class='flex jc_space-between'>
+                            <div class='tour_info_name'>$row[name_tour]</div>
+                            <div>
+                                <div class='tour_info_time'>$row[duration_tour]</div>
+                                <div class='tour_info_price'>$row[price_tour]₽</div>
+                            </div>
+                        </div>
+                        <div class='tour_desc'>$row[desc_tour]</div>
+                    </div>
+                </div>
+                ";
+        }
+    }
+?>
 
 <script>
     // $(".tour").hover(function() {
@@ -103,4 +46,7 @@
     // /*unhover*/ function() {
     //     $(this).find(".tour_desc").fadeOut();
     // });
+    function transition(id) {
+        $( "main" ).load( "include/tour_description.php", {id: id} );
+    }
 </script>
--- include/tours_page.php
+++ include/tours_page.php
@@ -5,113 +5,115 @@
             <img src="media/images/filters.svg" alt="Фильтры">
             Фильтры
         </div>
-        <dialog id="dialog_filters">
-            <div id="modal_content" class="modal">
-                <div class="filters_row">
-                    <div class="filters_col">
-                        <div class="filters_col_title">Место</div>
-                        <div class="input_wrapper">
-                            <label for="place_izh">Ижевск</label>
-                            <input id="place_izh" type="checkbox" value="Ижевск">
+        <form method="POST" id="filter_form">
+            <dialog id="dialog_filters">
+                <div id="modal_content" class="modal">
+                    <div class="filters_row">
+                        <div class="filters_col">
+                            <div class="filters_col_title">Место</div>
+                            <div class="input_wrapper">
+                                <label for="place_izh">Ижевск</label>
+                                <input name="place[]" id="place_izh" type="checkbox" value="Ижевск">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="place_vot">Воткинск</label>
+                                <input name="place[]" id="place_vot" type="checkbox" value="Воткинск">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="place_sar">Сарапул</label>
+                                <input name="place[]" id="place_sar" type="checkbox" value="Сарапул">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="place_glz">Глазов</label>
+                                <input name="place[]" id="place_glz" type="checkbox" value="Глазов">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="place_mzh">Можга</label>
+                                <input name="place[]" id="place_mzh" type="checkbox" value="Можга">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="place_cum">Камбарка</label>
+                                <input name="place[]" id="place_cum" type="checkbox" value="Камбарка">
+                            </div>
                         </div>
-                        <div class="input_wrapper">
-                            <label for="place_vot">Воткинск</label>
-                            <input id="place_vot" type="checkbox" value="Воткинск">
+                        <div class="filters_col">
+                            <div class="filters_col_title">Длительность</div>
+                            <div class="input_wrapper">
+                                <label for="time_hour">1 час</label>
+                                <input id="time_hour" type="checkbox" value="1h">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="time_hour3">3 час</label>
+                                <input id="time_hour3" type="checkbox" value="3h">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="time_day">1 день</label>
+                                <input id="time_day" type="checkbox" value="1d">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="time_day3">3 дня</label>
+                                <input id="time_day3" type="checkbox" value="3d">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="time_week">Неделя</label>
+                                <input id="time_week" type="checkbox" value="1w">
+                            </div>
                         </div>
-                        <div class="input_wrapper">
-                            <label for="place_sar">Сарапул</label>
-                            <input id="place_sar" type="checkbox" value="Сарапул">
+                        <div class="filters_col">
+                            <div class="filters_col_title">Возрастной рейтинг</div>
+                            <div class="input_wrapper">
+                                <label for="time_hour">0+</label>
+                                <input name="age[]" id="time_hour" type="checkbox" value="0+">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="time_hour3">6+</label>
+                                <input name="age[]" id="time_hour3" type="checkbox" value="6+">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="time_day">12+</label>
+                                <input name="age[]" id="time_day" type="checkbox" value="12+">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="time_day3">16+</label>
+                                <input name="age[]" id="time_day3" type="checkbox" value="16+">
+                            </div>
+                            <div class="input_wrapper">
+                                <label for="time_week">18+</label>
+                                <input name="age[]" id="time_week" type="checkbox" value="18+">
+                            </div>
                         </div>
-                        <div class="input_wrapper">
-                            <label for="place_glz">Глазов</label>
-                            <input id="place_glz" type="checkbox" value="Глазов">
-                        </div>
-                        <div class="input_wrapper">
-                            <label for="place_mzh">Можга</label>
-                            <input id="place_mzh" type="checkbox" value="Можга">
-                        </div>
-                        <div class="input_wrapper">
-                            <label for="place_cum">Камбарка</label>
-                            <input id="place_cum" type="checkbox" value="Камбарка">
+                        <div class="filters_col">
+                            <div class="filters_col_title">Цена</div>
+                            <div class="input_price">
+                                <label for="price_low">От: </label>
+                                <input id="price_low" type="number" min="100" max="25000" value="100">
+                            </div>
+                            <div class="input_price">
+                                <label for="price_high">До: </label>
+                                <input id="price_high" type="number" min="100" max="25000" value="25000">
+                            </div>
                         </div>
                     </div>
-                    <div class="filters_col">
-                        <div class="filters_col_title">Длительность</div>
-                        <div class="input_wrapper">
-                            <label for="time_hour">1 час</label>
-                            <input id="time_hour" type="checkbox" value="1h">
-                        </div>
-                        <div class="input_wrapper">
-                            <label for="time_hour3">3 час</label>
-                            <input id="time_hour3" type="checkbox" value="3h">
-                        </div>
-                        <div class="input_wrapper">
-                            <label for="time_day">1 день</label>
-                            <input id="time_day" type="checkbox" value="1d">
-                        </div>
-                        <div class="input_wrapper">
-                            <label for="time_day3">3 дня</label>
-                            <input id="time_day3" type="checkbox" value="3d">
-                        </div>
-                        <div class="input_wrapper">
-                            <label for="time_week">Неделя</label>
-                            <input id="time_week" type="checkbox" value="1w">
-                        </div>
-                    </div>
-                    <div class="filters_col">
-                        <div class="filters_col_title">Возрастной рейтинг</div>
-                        <div class="input_wrapper">
-                            <label for="time_hour">0+</label>
-                            <input id="time_hour" type="checkbox" value="1h">
-                        </div>
-                        <div class="input_wrapper">
-                            <label for="time_hour3">6+</label>
-                            <input id="time_hour3" type="checkbox" value="3h">
-                        </div>
-                        <div class="input_wrapper">
-                            <label for="time_day">14+</label>
-                            <input id="time_day" type="checkbox" value="1d">
-                        </div>
-                        <div class="input_wrapper">
-                            <label for="time_day3">16+</label>
-                            <input id="time_day3" type="checkbox" value="3d">
-                        </div>
-                        <div class="input_wrapper">
-                            <label for="time_week">18+</label>
-                            <input id="time_week" type="checkbox" value="1w">
-                        </div>
-                    </div>
-                    <div class="filters_col">
-                        <div class="filters_col_title">Цена</div>
-                        <div class="input_price">
-                            <label for="price_low">От: </label>
-                            <input id="price_low" type="number" min="100" max="25000" value="100">
-                        </div>
-                        <div class="input_price">
-                            <label for="price_high">До: </label>
-                            <input id="price_high" type="number" min="100" max="25000" value="25000">
-                        </div>
-                    </div>
-                </div>
-                <div class="filters_controls">
-                    <button class="filters_reset">
-                        <svg id="svg_filters_reset" fill="#ffffff" width="16px" height="16px" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><path d="M64,256H34A222,222,0,0,1,430,118.15V85h30V190H355V160h67.27A192.21,192.21,0,0,0,256,64C150.13,64,64,150.13,64,256Zm384,0c0,105.87-86.13,192-192,192A192.21,192.21,0,0,1,89.73,352H157V322H52V427H82V393.85A222,222,0,0,0,478,256Z"/></svg>
-                        Сбросить
-                    </button>
-                    <div class="flex">
-                        <form method="dialog">
-                            <button class="filters_cancel">Отмена</button>
-                        </form>
-                        <button class="filters_confirm">
-                            <svg id="svg_filters_confirm" fill="#ffffff" width="16px" height="16px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="m16 0c8.836556 0 16 7.163444 16 16s-7.163444 16-16 16-16-7.163444-16-16 7.163444-16 16-16zm0 2c-7.7319865 0-14 6.2680135-14 14s6.2680135 14 14 14 14-6.2680135 14-14-6.2680135-14-14-14zm6.6208153 9.8786797c.3905243.3905242.3905243 1.0236892 0 1.4142135l-7.0710678 7.0710678c-.3626297.3626297-.9344751.3885319-1.3269928.0777064l-.0872208-.0777064-4.24264068-4.2426407c-.39052429-.3905242-.39052429-1.0236892 0-1.4142135.39052428-.3905243 1.02368928-.3905243 1.41421358 0l3.5348268 3.5348268 6.3646681-6.3632539c.3905243-.3905243 1.0236893-.3905243 1.4142136 0z"/></svg>
-                            Применить
+                    <div class="filters_controls">
+                        <button class="filters_reset">
+                            <svg id="svg_filters_reset" fill="#ffffff" width="16px" height="16px" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><path d="M64,256H34A222,222,0,0,1,430,118.15V85h30V190H355V160h67.27A192.21,192.21,0,0,0,256,64C150.13,64,64,150.13,64,256Zm384,0c0,105.87-86.13,192-192,192A192.21,192.21,0,0,1,89.73,352H157V322H52V427H82V393.85A222,222,0,0,0,478,256Z"/></svg>
+                            Сбросить
                         </button>
+                        <div class="flex">
+                            <form method="dialog">
+                                <button class="filters_cancel">Отмена</button>
+                            </form>
+                            <button class="filters_confirm">
+                                <svg id="svg_filters_confirm" fill="#ffffff" width="16px" height="16px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="m16 0c8.836556 0 16 7.163444 16 16s-7.163444 16-16 16-16-7.163444-16-16 7.163444-16 16-16zm0 2c-7.7319865 0-14 6.2680135-14 14s6.2680135 14 14 14 14-6.2680135 14-14-6.2680135-14-14-14zm6.6208153 9.8786797c.3905243.3905242.3905243 1.0236892 0 1.4142135l-7.0710678 7.0710678c-.3626297.3626297-.9344751.3885319-1.3269928.0777064l-.0872208-.0777064-4.24264068-4.2426407c-.39052429-.3905242-.39052429-1.0236892 0-1.4142135.39052428-.3905243 1.02368928-.3905243 1.41421358 0l3.5348268 3.5348268 6.3646681-6.3632539c.3905243-.3905243 1.0236893-.3905243 1.4142136 0z"/></svg>
+                                Применить
+                            </button>
+                        </div>
+                        
                     </div>
                     
                 </div>
-                
-            </div>
-        </dialog>
+            </dialog>
+        </form>
         <div class="tours_list">
             <?include 'include/tours_list.php'?>       
         </div>
@@ -120,4 +122,12 @@
 
 <script>
     document.getElementById("tours").classList.add("btn_header_active");
+
+    $(".filters_confirm").click(function() {
+        var setdata = $("#filter_form").serialize();    
+        $.post('include/tours_list_edit.php', setdata, function(data) {
+            $('.tours_list').html(data);
+        });
+        event.preventDefault();
+    });
 </script>
